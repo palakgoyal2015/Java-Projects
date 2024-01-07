f7b7bf2207916dccc68810d729f870760c0d54f3
palak goyal <palak2015.be22@chitkara.edu.in>
2024-01-07 19:40:55 +0530

    First Commit

---
+++ src/com/learing/demo.java
@@ -0,0 +1,32 @@
+//what is java
+// Java is a programming language and computing platform first released by Sun Microsystems in 1995. It has evolved from humble beginnings to power a large share of today’s 
+// digital world, by providing the reliable platform upon which many services and applications are built. New, innovative products and digital services designed for the 
+// future continue to rely on Java, as well.
+//features of java 
+//1)object-oriented
+//2)platform independent
+//3)multi threaded
+//4)secure
+//5)high performance
+
+package com.learing;
+
+public class demo {
+    public static void main(String[] args){
+        String name="palak";
+        String Father_name="rakesh";
+        String mother_name="anju";
+        int age=19;
+        String Gender="Female";
+        String address="nai basti";
+        int mobile_no=123456789;
+        System.out.println("Name:"+name);
+        System.out.println("Father's Name:"+Father_name);
+        System.out.println("Mother's Name:"+mother_name);
+        System.out.println("Age:"+age);
+        System.out.println("Gender:"+Gender);
+        System.out.println("Address:"+address);
+        System.out.println("Mobile no:"+mobile_no);
+
+    }
+}
